6ea58f160573f26393a1790fa1e5100527dcf8d9
Kanatbek-AKA <utrechtenov@gmail.com>
2023-03-06 18:51:58 +0000

    Need to fix

---
+++ onlinecourse/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+# <HINT> Import any new Models here
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission, examGrades
+
+# <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInLine(admin.StackedInline):
+    model = Question
+    extra = 6                    
+    
+class ChoiceInline(admin.StackedInline):
+    model = Choice 
+    extra = 3
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+        
+# Additional 
+class ExameGradeInline(admin.StackedInline):
+    list_display = ['course', 'exam_question', 'exam_answer', 'grade']
+    model = examGrades
+        
+    
+# Register your models here.
+@admin.register(Course)                       # define register of Course here using @
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+# <HINT> Register Question and Choice models here
+admin.site.register(Question)  #
+admin.site.register(Choice)    # 
+# admin.site.register(Course, CourseAdmin)     # Or define register of Course here
+# admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+#
+admin.site.register(examGrades)
---
+++ static/javascript/course.js
@@ -0,0 +1,26 @@
+// "use strict";
+
+// TODO
+// window.onload = function () {
+//   const warning_btn = document.querySelector("#exam_warn");
+//   const txt = document.querySelector("#txt_show");
+//   warning_btn.addEventListener("click", function (elem) {
+//     elem.preventDefault();
+//     const checkbox = document.querySelectorAll(".form-check-input");
+//     let checked = false;
+//     for (let i = 0; i < checkbox.length; i++) {
+//       if (checkbox[i].checked) {
+//         checked = true;
+//         break;
+//       }
+//     }
+//     if (checked) {
+//       txt.innerHTML =
+//         "<strong style='color:red;'>Now you can submit your exam answer by click on Submit button below.</strong>";
+//       // document.querySelector('form').submit();
+//     } else {
+//       txt.textContent = "Please check the boxes to submit your answer";
+//       //   txt.classList.toggle("exam_warn--hidden");
+//     }
+//   });
+// };
